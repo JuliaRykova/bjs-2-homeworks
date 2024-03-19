7739c4f5aa13483a79e00ac3e02871ca5377c81f
Julia Rykova <yuliya.yurevna.01@inbox.ru>
2024-03-19 21:48:01 +0300

    выполнила задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant;
+  let calculateDiscriminant = (b ** 2) - 4 * a * c;
+  if (calculateDiscriminant < 0) {
+  } else if (calculateDiscriminant === 0) {
+    discriminant = -b / (2 * a);
+    arr.push(discriminant);
+  } else if (calculateDiscriminant > 0) {
+    discriminant = (-b + Math.sqrt(calculateDiscriminant)) / (2 * a);
+    arr.push(discriminant);
+    discriminant = (-b - Math.sqrt(calculateDiscriminant)) / (2 * a);
+    arr.push(discriminant);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyInterestRate = percent / 100 / 12;
+  let loanBalance = amount - contribution;
+  let monthlyPayment = loanBalance * (monthlyInterestRate + monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, countMonths) - 1));
+  let totalPayment = monthlyPayment * countMonths;
+  return +totalPayment.toFixed(2);
 }
