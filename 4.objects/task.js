aeec130f5fc161909855edcb7b24a8acd1794d4c
Julia Rykova <yuliya.yurevna.01@inbox.ru>
2024-03-28 19:59:38 +0300

    сделала

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+    }
+    
+    Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+    };
+    
+    Student.prototype.addMarks = function (...marks) {
+    if (marks.length > 0) {
+    this.marks.push(...marks);
+    }
+    };
+    
+    Student.prototype.getAverage = function () {
+    if (this.marks.length > 0) {
+    let result = this.marks.reduce((a, b) => a + b);
+    return result / this.marks.length;
+    } else {
+    return 0;
+    }
+    };
+    
+    Student.prototype.exclude = function (reason) {
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
+    };
+    
+    
