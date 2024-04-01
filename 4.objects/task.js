42364b123ee727bb34b00e54c963ba6d093c67c9
JuliaRykova <114673572+JuliaRykova@users.noreply.github.com>
2024-04-01 10:06:28 +0300

    Update task.js
    исправила

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,33 +1,31 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
-    }
-    
-    Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
-    };
-    
-    Student.prototype.addMarks = function (...marks) {
-    if (marks.length > 0) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+}
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+};
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.hasOwnProperty('marks')) {
     this.marks.push(...marks);
-    }
-    };
-    
-    Student.prototype.getAverage = function () {
-    if (this.marks.length > 0) {
+  }
+};
+
+Student.prototype.getAverage = function () {
+  if (this.hasOwnProperty('marks')) {
     let result = this.marks.reduce((a, b) => a + b);
     return result / this.marks.length;
-    } else {
+  } else {
     return 0;
-    }
-    };
-    
-    Student.prototype.exclude = function (reason) {
-    this.excluded = reason;
-    delete this.subject;
-    delete this.marks;
-    };
-    
-    
+  }
+};
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+};
